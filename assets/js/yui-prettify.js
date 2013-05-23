3b093667c21c5769e9227679869b0e4bf7832acf
Zeno Rocha <zno.rocha@gmail.com>
2013-05-23 17:40:39 -0300

    Fix JSHint problem

--- assets/js/yui-prettify.js
+++ assets/js/yui-prettify.js
@@ -1,12 +1,13 @@
 YUI().use('node', function(Y) {
     var h,
+        l,
         lis,
         code = Y.all('.prettyprint.linenums');
 
     if (code.size()) {
         code.each(function(c) {
-            lis = c.all('ol li'),
-                l = 1;
+            lis = c.all('ol li');
+            l = 1;
             lis.each(function(n) {
                 n.prepend('<a name="LINENUM_' + l + '"></a>');
                 l++;
